cdf50d4c2881d56b982adcaef869c13a200ecb05
Aleksandr Krasikov <Sirius1943@yandex.ru>
2023-11-13 21:11:07 +0300

    First commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+
+        Cat one = (Cat) applicationContext.getBean("cat");
+        System.out.println(one.getName());
+        Cat two = (Cat) applicationContext.getBean("cat");
+        System.out.println(two.getName());
+
+        System.out.println(bean == beanTwo);
+        System.out.println(one == two);
     }
 }
